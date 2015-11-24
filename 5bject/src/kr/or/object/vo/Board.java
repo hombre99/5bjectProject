3b883da26f0a432cc1b3037383b115d41a69822e
kosta <kosta@kosta-HP>
2015-11-24 16:02:52 +0900

    20151124.LC.Board Vo 수정 외

--- 5bject/src/kr/or/object/vo/Board.java
+++ 5bject/src/kr/or/object/vo/Board.java
@@ -9,134 +9,146 @@ public class Board{
    private String title;
    private String content;
    private Date date;
+   private int notice;
    private int hit;
    private int ref;
    
    public Board(){}
 
-   public Board(int writeNo, String id, String title, String content, Date date, int hit, int ref) {
-      super();
-      this.writeNo = writeNo;
-      this.id = id;
-      this.title = title;
-      this.content = content;
-      this.date = date;
-      this.hit = hit;
-      this.ref = ref;
-   }
+	public Board(int writeNo, String id, String title, String content, Date date, int notice, int hit, int ref) {
+		super();
+		this.writeNo = writeNo;
+		this.id = id;
+		this.title = title;
+		this.content = content;
+		this.date = date;
+		this.notice = notice;
+		this.hit = hit;
+		this.ref = ref;
+	}
 
-   public int getWriteNo() {
-      return writeNo;
-   }
+	public int getWriteNo() {
+		return writeNo;
+	}
 
-   public void setWriteNo(int writeNo) {
-      this.writeNo = writeNo;
-   }
+	public void setWriteNo(int writeNo) {
+		this.writeNo = writeNo;
+	}
 
-   public String getId() {
-      return id;
-   }
+	public String getId() {
+		return id;
+	}
 
-   public void setId(String id) {
-      this.id = id;
-   }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-   public String getTitle() {
-      return title;
-   }
+	public String getTitle() {
+		return title;
+	}
 
-   public void setTitle(String title) {
-      this.title = title;
-   }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-   public String getContent() {
-      return content;
-   }
+	public String getContent() {
+		return content;
+	}
 
-   public void setContent(String content) {
-      this.content = content;
-   }
+	public void setContent(String content) {
+		this.content = content;
+	}
 
-   public Date getDate() {
-      return date;
-   }
+	public Date getDate() {
+		return date;
+	}
 
-   public void setDate(Date date) {
-      this.date = date;
-   }
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
-   public int getHit() {
-      return hit;
-   }
+	public int getNotice() {
+		return notice;
+	}
 
-   public void setHit(int hit) {
-      this.hit = hit;
-   }
+	public void setNotice(int notice) {
+		this.notice = notice;
+	}
 
-   public int getRef() {
-      return ref;
-   }
+	public int getHit() {
+		return hit;
+	}
 
-   public void setRef(int ref) {
-      this.ref = ref;
-   }
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
 
-   @Override
-   public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((content == null) ? 0 : content.hashCode());
-      result = prime * result + ((date == null) ? 0 : date.hashCode());
-      result = prime * result + hit;
-      result = prime * result + ((id == null) ? 0 : id.hashCode());
-      result = prime * result + ref;
-      result = prime * result + ((title == null) ? 0 : title.hashCode());
-      result = prime * result + writeNo;
-      return result;
-   }
+	public int getRef() {
+		return ref;
+	}
 
-   @Override
-   public boolean equals(Object obj) {
-      if (this == obj)
-         return true;
-      if (obj == null)
-         return false;
-      if (getClass() != obj.getClass())
-         return false;
-      Board other = (Board) obj;
-      if (content == null) {
-         if (other.content != null)
-            return false;
-      } else if (!content.equals(other.content))
-         return false;
-      if (date == null) {
-         if (other.date != null)
-            return false;
-      } else if (!date.equals(other.date))
-         return false;
-      if (hit != other.hit)
-         return false;
-      if (id == null) {
-         if (other.id != null)
-            return false;
-      } else if (!id.equals(other.id))
-         return false;
-      if (ref != other.ref)
-         return false;
-      if (title == null) {
-         if (other.title != null)
-            return false;
-      } else if (!title.equals(other.title))
-         return false;
-      if (writeNo != other.writeNo)
-         return false;
-      return true;
-   }
+	public void setRef(int ref) {
+		this.ref = ref;
+	}
 
-   @Override
-   public String toString() {
-      return "Board [writeNo=" + writeNo + ", id=" + id + ", title=" + title + ", content=" + content + ", date="
-            + date + ", hit=" + hit + ", ref=" + ref + "]";
-   }
-   
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + hit;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + notice;
+		result = prime * result + ref;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + writeNo;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Board other = (Board) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (hit != other.hit)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (notice != other.notice)
+			return false;
+		if (ref != other.ref)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (writeNo != other.writeNo)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Board [writeNo=" + writeNo + ", id=" + id + ", title=" + title + ", content=" + content + ", date="
+				+ date + ", notice=" + notice + ", hit=" + hit + ", ref=" + ref + "]";
+	}   
 }
