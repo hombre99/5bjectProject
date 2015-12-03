8226e484386fbec207a7297f03ab588bcc9e1d8f
Hyunji Cha <hyunji3190@gmail.com>
2015-12-03 13:41:10 +0900

    font-weight bold도 하나 더 추가. chj

--- 5bject/WebContent/WEB-INF/script/game/game3.js
+++ 5bject/WebContent/WEB-INF/script/game/game3.js
@@ -252,6 +252,7 @@ $(document).ready(function(){
 				var gameWord = document.createTextNode(gameWords[gameWordsIndex]);
 				$(td).css("word-break", "break-all");
 				$(td).css("text-align", "center");
+				$(td).css("font-weight", "bold");
 				$(td).css("width", (maxTableWidth / difficulty) + "px").css("height", (maxTableHeight / difficulty) + "px").append(gameWord);
 				$(tr).append(td);
 				gameWordsIndex++;
